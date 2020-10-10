cd31e4d73bd6fcee8cc41917a2ae81031901ed5f
TAKAMI Torao <koiroha@gmail.com>
2020-10-10 19:00:47 +0900

    M

--- carillon-core/src/context.rs
+++ carillon-core/src/context.rs
@@ -21,14 +21,14 @@ impl<'ctx> Context<'ctx> {
 
     log::debug!("Loading the configuration: {}", dir.to_string_lossy());
     let conf_file = dir.join("carillon.toml");
-    let conf = std::fs::read_to_string(conf_file)?;
+    let conf = std::fs::read_to_string(conf_file.as_path())?;
     let conf: Settings = toml::from_str(conf.as_str()).map_err(|err| {
       let position = err.line_col();
       Detail::InvalidConfig {
         message: err.to_string(),
-        location: dir.to_string_lossy().to_string(),
-        line: position.map(|x| x.0 as u64).unwrap_or(0u64),
-        column: position.map(|x| x.1 as u64).unwrap_or(0u64),
+        location: conf_file.to_string_lossy().to_string(),
+        line: position.map(|x| x.0 as u64 + 1).unwrap_or(0u64),
+        column: position.map(|x| x.1 as u64 + 1).unwrap_or(0u64),
       }
     })?;
     Ok(Box::new(Context { dir: Box::new(dir.clone()), settings: Box::new(conf) }))
@@ -50,13 +50,10 @@ struct Node {
 }
 
 #[derive(Debug, Deserialize)]
-struct Identity {
-  method: Option<String>,
-  private_key: Option<PrivateKey>,
-}
-
-#[derive(Debug, Deserialize)]
-struct PrivateKey {
-  algorithm: Option<String>,
-  location: Option<String>,
+enum Identity {
+  #[serde(rename="private_key")]
+  PrivateKey {
+    algorithm: String,
+    location: String
+  },
 }
