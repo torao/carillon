1f8bae6124c6286a7a93c96f4273b1474eeffac2
TAKAMI Torao <torao.takami@linecorp.com>
2020-10-10 18:40:10 +0900

    Contextからアドレスを取得できるように修正中

--- carillon-core/src/context.rs
+++ carillon-core/src/context.rs
@@ -4,8 +4,12 @@ use serde::Deserialize;
 use toml;
 
 use crate::error::{Detail, Result};
+use crate::security::{KeyPair, PublicKeyImpl};
+use crate::security::ed25519;
+use crate::tools::init::{DEFAULT_KEY_ALGORITHM, DEFAULT_KEY_LOCATION};
 
 pub const DIR_SECURITY: &str = "security";
+pub const DEFAULT_IDENTITY_METHOD: &str = "private_key";
 
 pub struct Context<'ctx> {
   dir: Box<&'ctx Path>,
@@ -33,6 +37,46 @@ impl<'ctx> Context<'ctx> {
     })?;
     Ok(Box::new(Context { dir: Box::new(dir.clone()), settings: Box::new(conf) }))
   }
+
+  fn public_key_algorithm(&self) -> Result<Box<PublicKeyImpl>> {
+    if let Some(identity) = &self.settings.node.identity.method {
+      if identity != DEFAULT_IDENTITY_METHOD {
+        return Err(Detail::UnsupportedSetting {
+          location: self.dir.to_string_lossy().to_string(),
+          item: "identity method",
+          value: identity.to_string(),
+        });
+      }
+    }
+    let algorithm_name = (if let Some(pk) = self.settings.node.identity.private_key {
+      pk.algorithm
+    } else {
+
+    }
+      .map(|pk| pk.algorithm)
+      .flatten()
+      .map(|a| a.to_string())
+      .unwrap_or(DEFAULT_KEY_ALGORITHM.to_string());
+    let algorithm = match algorithm_name.as_str() {
+      "ed25519" => ed25519::algorithm(),
+      unsupported => {
+        return Err(Detail::UnsupportedSetting {
+          location: self.dir.to_string_lossy().to_string(),
+          item: "public-key algorithm",
+          value: unsupported.to_string(),
+        });
+      }
+    };
+    Ok(Box::new(algorithm))
+  }
+
+  fn key_pair(&self) -> Result<Box<dyn KeyPair>> {
+    let algorithm = self.public_key_algorithm()?;
+    let location = self.settings.node.identity.private_key.map(|s| s.location).flatten().unwrap_or(DEFAULT_KEY_LOCATION.to_string());
+    let path = Path::new(location.as_str());
+    let bytes = std::fs::read(path)?;
+    algorithm.restore_key_pair(&bytes)
+  }
 }
 
 pub fn localnode_key_pair_file(key_algorithm: &str) -> String {
@@ -55,7 +99,7 @@ struct Identity {
   private_key: Option<PrivateKey>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 struct PrivateKey {
   algorithm: Option<String>,
   location: Option<String>,
--- carillon-core/src/security/ed25519/mod.rs
+++ carillon-core/src/security/ed25519/mod.rs
@@ -1,27 +1,46 @@
 use ed25519_dalek::{Signer, Verifier};
 use rand::{CryptoRng, RngCore};
+use rand::prelude::StdRng;
+use rand::rngs::OsRng;
+use rand::SeedableRng;
 
 use crate::error::Detail;
-use crate::security;
 use crate::Result;
+use crate::security;
+use crate::security::PublicKeyImpl;
 
 const SIGNATURE_SIZE: usize = 64;
 
-pub struct Ed25519 {}
-
-impl security::Algorithm for Ed25519 {
-  fn id() -> &'static str {
-    "ed25519"
+pub fn algorithm() -> PublicKeyImpl {
+  PublicKeyImpl {
+    id: "ed25519",
+    generate_key_pair,
+    generate_key_pair_from,
+    restore_key_pair,
   }
 }
 
-impl security::PublicKeyAlgorithm for Ed25519 {
-  fn generate_key_pair_from<CSPRNG>(csprng: &mut CSPRNG) -> Box<dyn security::KeyPair>
-  where
-    CSPRNG: CryptoRng + RngCore,
-  {
-    let key_pair = ed25519_dalek::Keypair::generate(csprng);
-    Box::new(KeyPair { key_pair })
+fn generate_key_pair() -> Box<dyn security::KeyPair> {
+  let mut csprng = OsRng {};
+  generate_key_pair_from_csprng(&mut csprng)
+}
+
+fn generate_key_pair_from(seed: u64) -> Box<dyn security::KeyPair> {
+  let mut csprng = StdRng::seed_from_u64(seed);
+  generate_key_pair_from_csprng(&mut csprng)
+}
+
+fn generate_key_pair_from_csprng<CSPRNG>(csprng: &mut CSPRNG) -> Box<dyn security::KeyPair>
+  where CSPRNG: CryptoRng + RngCore
+{
+  let key_pair = ed25519_dalek::Keypair::generate(csprng);
+  Box::new(KeyPair { key_pair })
+}
+
+fn restore_key_pair(bytes: &[u8]) -> Result<Box<dyn security::KeyPair>> {
+  match ed25519_dalek::Keypair::from_bytes(bytes) {
+    Ok(key_pair) => Ok(Box::new(KeyPair { key_pair })),
+    Err(err) => Err(Detail::CannotRestoreKey { message: err.to_string() })
   }
 }
 
@@ -32,15 +51,6 @@ pub struct KeyPair {
 impl security::KeyPair for KeyPair {
   fn to_bytes(&self) -> Vec<u8> {
     self.key_pair.to_bytes().to_vec()
-  }
-
-  fn from_bytes(bytes: &[u8]) -> Result<Self>
-  where
-    Self: Sized,
-  {
-    ed25519_dalek::Keypair::from_bytes(bytes)
-      .map(|key_pair| KeyPair { key_pair })
-      .map_err(|err| Detail::CannotRestoreKey { message: err.to_string() })
   }
 
   fn public_key(&self) -> Box<dyn security::PublicKey> {
@@ -65,8 +75,8 @@ impl security::PublicKey for PublicKey {
     self.public_key.to_bytes().to_vec()
   }
   fn from_bytes(bytes: &[u8]) -> Result<Self>
-  where
-    Self: Sized,
+    where
+      Self: Sized,
   {
     match ed25519_dalek::PublicKey::from_bytes(bytes) {
       Ok(public_key) => Ok(PublicKey { public_key }),
